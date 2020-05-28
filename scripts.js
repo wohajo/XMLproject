d7f131187272be936315885248b6be540eecc7ea
wohajo <michalos222@gmail.com>
2020-05-29 00:27:29 +0200

    final commit, working add buttons for every category

--- scripts.js
+++ scripts.js
@@ -3,10 +3,23 @@ document.getElementById("search-product-id").addEventListener("keyup", filterPro
 document.getElementById("search-model").addEventListener("keyup", filterProducts)
 document.getElementById("search-detailed").addEventListener("keyup", filterProducts)
 document.getElementById("clear-products-filter-button").addEventListener("click", clearProductsFilters)
+initializeButtons()
 
-var removeProductButtons = document.getElementsByClassName("delete-button");
-for (var i = 0; i < removeProductButtons.length; i++) {
-    removeProductButtons[i].addEventListener("click", removeProduct, false)
+function initializeButtons() {
+    var updateProductsButtons = document.getElementsByClassName("update-button");
+    for (var i = 0; i < updateProductsButtons.length; i++) {
+        updateProductsButtons[i].addEventListener("click", updateProduct, false)
+    }
+
+    var removeProductButtons = document.getElementsByClassName("delete-button");
+    for (var i = 0; i < removeProductButtons.length; i++) {
+        removeProductButtons[i].addEventListener("click", removeProduct, false)
+    }
+}
+
+var addProductsButtons = document.getElementsByClassName("add-button");
+for (var i = 0; i < addProductsButtons.length; i++) {
+    addProductsButtons[i].addEventListener("click", addProduct, false)
 }
 
 function filterProducts() {
@@ -49,11 +62,6 @@ function removeProduct() {
     event.currentTarget.parentElement.parentElement.parentElement.remove();
 }
 
-var updateProductsButtons = document.getElementsByClassName("update-button");
-for (var i = 0; i < updateProductsButtons.length; i++) {
-    updateProductsButtons[i].addEventListener("click", updateProduct, false)
-}
-
 function updateProduct() {
     var button = event.currentTarget;
     var brands = event.currentTarget.parentElement.parentElement.parentElement.getElementsByClassName("brand-search-target");
@@ -81,13 +89,37 @@ function updateProduct() {
     }
 }
 
-var addProductsButtons = document.getElementsByClassName("add-button");
-for (var i = 0; i < addProductsButtons.length; i++) {
-    addProductsButtons[i].addEventListener("click", addProduct, false)
-}
-
 function addProduct() {
     var buttonId = event.currentTarget.id;
+    var detailedDiv
+    var idPrefix
+    var productsElement
+
+    if(buttonId == "add-guitar-button") {
+        detailedDiv = '<div class="card-detailed-info"><p class="detailed-search-target">pickups: </p></div>'
+        idPrefix = "gui-"
+        productsElement = "guitars"
+    } else if(buttonId == "add-bass-button") {
+        detailedDiv = '<div class="card-detailed-info"><p class="detailed-search-target">pickups: </p></div>'
+        idPrefix = "bas-"
+        productsElement = "basses"
+    } else if (buttonId == "add-amplifier-button") {
+        detailedDiv = '<div class="card-detailed-info">speakers: <p class="detailed-search-target"> </p></div>'
+        idPrefix = "amp-"
+        productsElement = "amplifiers"
+    } else if (buttonId == "add-strings-button") {
+        detailedDiv = '<div class="card-detailed-info">size: <p class="detailed-search-target"> </p></div>'
+        idPrefix = "spk-"
+        productsElement = "strings"
+    } else if (buttonId == "add-picks-button") {
+        detailedDiv = '<div class="card-detailed-info">width: <p class="detailed-search-target"> </p></div>'
+        idPrefix = "pic-"
+        productsElement = "picks"
+    } else {
+        detailedDiv = '<div class="card-detailed-info"><p class="detailed-search-target">Type of effect: <br/>Voltage required: </p></div>'
+        idPrefix = "efc-"
+        productsElement = "effects"
+    }
 
     // product card
     var productCardDiv = document.createElement("div")
@@ -101,27 +133,25 @@ function addProduct() {
     var cardIdP = document.createElement("p")
     cardIdDiv.appendChild(cardIdP)
     cardIdP.classList.add("product-id-search-target")
-    var newIdNumber = document.getElementById("guitars").childElementCount + 1
-    cardIdP.innerText = "gui-" + newIdNumber
+    var newIdNumber = document.getElementById(productsElement).childElementCount + 1
+    cardIdP.innerText = idPrefix + newIdNumber
     // title div  
-    productCardDiv.innerHTML += '<div class="card-title-info"> brand: <p class="brand-search-target">New guitar</p> model: <p class="model-search-target">New model</p></div>'
+    productCardDiv.innerHTML += '<div class="card-title-info"> brand: <p class="brand-search-target">New item</p> model: <p class="model-search-target">New model</p></div>'
     // detailed info
-    productCardDiv.innerHTML += ' <div class="card-detailed-info"><p class="detailed-search-target">Detailed informations</p></div>'
+    productCardDiv.innerHTML += detailedDiv
     // price
     productCardDiv.innerHTML += '<div class="card-price-info">price: <p><input readonly="true" class="price-input" type="number" value="0" oninput="validity.valid||(value=\'\');"/></p></div>'
     // buttons 
     productCardDiv.innerHTML += '<div class="product-buttons"><p class="img-paragraph"><button class="product-button update-button update-button-guitar"><img src="https://img.icons8.com/material-rounded/24/000000/edit.png"/></button></p><p class="img-paragraph"><button class="product-button delete-button"><img src="https://img.icons8.com/material-rounded/24/000000/trash.png"/></button></p></div>'
 
-    var guitars = document.getElementById("guitars");
-    guitars.appendChild(productCardDiv);
+    var products = document.getElementById(productsElement);
+    products.appendChild(productCardDiv);
 
-    var updateProductsButtons = document.getElementsByClassName("update-button");
-    for (var i = 0; i < updateProductsButtons.length; i++) {
-        updateProductsButtons[i].addEventListener("click", updateProduct, false)
-    }
-
-    var removeProductButtons = document.getElementsByClassName("delete-button");
-    for (var i = 0; i < removeProductButtons.length; i++) {
-        removeProductButtons[i].addEventListener("click", removeProduct, false)
-    }
+    // buttons to update
+    var children = productCardDiv.childNodes
+    var buttonsP = children[children.length - 1].childNodes
+    var imgDiv1 = buttonsP[0].childNodes
+    var imgDiv2 = buttonsP[1].childNodes
+    imgDiv1[0].addEventListener("click", updateProduct, false)
+    imgDiv2[0].addEventListener("click", removeProduct, false)
 }
